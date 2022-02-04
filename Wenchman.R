2b9fe3e8f22e55a601d5dd4deb68a6b59b6a8d7b
John Froeschke <john.froeschke@gmail.com>
2022-02-04 14:49:51 -0500

    completed data cleaning and shapefile export

--- Wenchman.R
+++ Wenchman.R
@@ -61,7 +61,7 @@ Species2 <- data.frame(Species2)
 
 ## SUBSET TO AQUILO & BURTI
 #df <- subset(bgsrec, SPEC_BGS=="AQUILO" | SPEC_BGS=="BURTI")
-df <- subset(bgsrec, SPEC_BGS=="AQUILO" )
+df <- subset(bgsrec, SPEC_BGS=="AQUILO" | SPEC_BGS=="BURTI" )
 ### merge df with starec2
 df2 <- merge(starec2, df, by=c("CRUISEID","STATIONID", "CRUISE_NO"),
             all=TRUE)
@@ -76,7 +76,7 @@ df3 <- dcast(df2, CRUISEID + STATIONID + CRUISE_NO + lat + long  + START_DATE +
                year + month + HAULVALUE
              ~ SPEC_BGS, 
              sum, value.var="CNT", fill=0)
-
+## Remove NA's
 df4 <-  df3[!(is.na(df3$lat)),]
 
 
@@ -122,72 +122,103 @@ leaflet() %>%
     options = layersControlOptions(collapsed = FALSE)
   )
 
+POS <- subset(df4, BURTI > 0)
+NEG <- subset(df4, BURTI == 0)
+
+##############map the POS data
 
 
-save.image("Wenchman.RData")
+POS.coords <- data.frame(x=POS$lon, y=POS$lat)
+coordinates(POS.coords) <- ~ x + y
+class(POS.coords)
+### Assign albers projection
+proj4string(POS.coords)  <- CRS("+proj=longlat +datum=WGS84")  ## for example
+plot(POS.coords, axes=TRUE) ## sanity check
+
+NEG.coords <- data.frame(x=NEG$lon, y=NEG$lat)
+coordinates(NEG.coords) <- ~ x + y
+class(NEG.coords)
+### Assign albers projection
+proj4string(NEG.coords)  <- CRS("+proj=longlat +datum=WGS84")  ## for example
+plot(NEG.coords, axes=TRUE) ## sanity check
+
+
+library(leaflet)
+leaflet(POS.coords) %>% 
+  addTiles('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+           options = providerTileOptions(noWrap = TRUE)) %>%
+  addTiles('http://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/Mapserver/tile/{z}/{y}/{x}',
+           options = providerTileOptions(noWrap = TRUE)) %>% 
+  addCircleMarkers(color = "#FFFF00", radius=3)
+
+
+leaflet() %>% 
+  addTiles('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+           options = providerTileOptions(noWrap = TRUE)) %>%
+  addTiles('http://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/Mapserver/tile/{z}/{y}/{x}',
+           options = providerTileOptions(noWrap = TRUE)) %>% 
+  addCircleMarkers(data=NEG.coords,color = "#9370DB", radius=1, group="Zero") %>% 
+  addCircleMarkers(data=POS.coords,color = "#FFFF00", radius=3, group="Wenchmen") %>% 
+  addLayersControl(
+    overlayGroups = c("Wenchman", "Zero"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
+
+POSAquilo <- subset(df4, AQUILO > 0)
+POSBurti <- subset(df4, BURTI > 0)
+
+##############map the POS data
+
+
+POSAquilo.coords <- data.frame(x=POSAquilo$lon, y=POSAquilo$lat)
+coordinates(POSAquilo.coords) <- ~ x + y
+class(POSAquilo.coords)
+### Assign albers projection
+proj4string(POSAquilo.coords)  <- CRS("+proj=longlat +datum=WGS84")  ## for example
+plot(POSAquilo.coords, axes=TRUE) ## sanity check
+
+POSBurti.coords <- data.frame(x=POSBurti$lon, y=POSBurti$lat)
+coordinates(POSBurti.coords) <- ~ x + y
+class(POSBurti.coords)
+### Assign albers projection
+proj4string(POSBurti.coords)  <- CRS("+proj=longlat +datum=WGS84")  ## for example
+plot(POSBurti.coords, axes=TRUE) ## sanity check
 
 
 
-################### Not used###########################
-             
-             unique + GEARS +
-#                 DECSLAT + DECSLON +
-#                 DEPTH_ESTA +
-#                 TIME_EMIL +
-#                 GEAR_SIZE + 
-#                 MESH_SIZE +
-#                 MO_DAY_YR + 
-#                 SURVEY +
-#                 DEPTH_SSTA
-#               ~ TAXONOMIC, sum, value.var="TOTCNT", fill=0) 
 
 
- 
+leaflet() %>% 
+  addTiles('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+           options = providerTileOptions(noWrap = TRUE)) %>%
+  addTiles('http://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/Mapserver/tile/{z}/{y}/{x}',
+           options = providerTileOptions(noWrap = TRUE)) %>% 
+  addCircleMarkers(data=POSBurti.coords,color = "#9370DB", radius=1, group="Butterfish") %>% 
+  addCircleMarkers(data=POSAquilo.coords,color = "#FFFF00", radius=3, group="Wenchman") %>% 
+  addLayersControl(
+    overlayGroups = c("Wenchman", "Butterfish"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
+
+
+######### convert df4 to shapefile
 
 ## Convert to SpatialPointsDataFrame
-df.coords2 <- df.coords
-df.coords3 <- SpatialPointsDataFrame(df.coords2, data=df.pos[,c(1,2,5)])
+
+df4.coords <- data.frame(x=df4$long, y=df4$lat)
+coordinates(df4.coords) <- ~ x + y
+
+proj4string(df4.coords)  <- CRS("+proj=longlat +datum=WGS84")  ## for example
+plot(df4.coords, axes=TRUE)
+
+library(sp)
+library(rgdal)
+df4sp <- SpatialPointsDataFrame(df4.coords, data=df4)
 ### Export object for backup.  Will need to test percent of points in final polygon
-writeOGR( df.coords3, layer="level",
-          "C:/EFHreview/Data/Large_environmental_database_kernel_density/shp_95_albers/test_points.shp",
+writeOGR( df4sp, layer="level",
+          "C:/Users/JohnF/OneDrive - Gulf of Mexico Fishery Mgmt Council/Documents/SEAMAP/shapefile/SEAMAP.shp",
           driver="ESRI Shapefile",
           overwrite_layer=TRUE)
 
-m4 <- merge(m3, PENAEUSbgs, by=c("CRUISEID","STATIONID", "CRUISE_NO"),
-            all.x=TRUE)
-## Section 3:
+save.image("C:/Users/JohnF/OneDrive - Gulf of Mexico Fishery Mgmt Council/Documents/SEAMAP/Wenchman/Wenchman.RData")
 
-
-##
-
-## Improve the dataset
-df <- data.frame(
-  CRUISEID = m4$CRUISEID,
-  STATIONID = m4$STATIONID,
-  GEARS = m4$GEARS,
-  DECSLAT = m4$DECSLAT,
-  DECSLON = m4$DECSLON,
-  CRUISE_NO = m4$CRUISE_NO,
-  MO_DAY_YR = m4$MO_DAY_YR,
-  YR = m4$YR,
-  DEPTH_SSTA = m4$DEPTH_SSTA,
-  DEPTH_ESTA = m4$DEPTH_ESTA,
-  TEMP_SSURF = m4$TEMP_SSURF,
-  TEMP_BOT = m4$TEMP_BOT,
-  TEMPSURF = m4$TEMPSURF,
-  TEMPMID = m4$TEMPMID,
-  TEMPMAX = m4$TEMPMAX,
-  SALSURF = m4$SALSURF,
-  SALMID = m4$SALMID,
-  SALMAX = m4$SALMAX,
-  CHLORSURF = m4$CHLORSURF,
-  CHLORMID = m4$CHLORMID,
-  CHLORMAX = m4$CHLORMAX,
-  OXYSURF = m4$OXYSURF,
-  OXYMID = m4$OXYMID,
-  OXYMAX = m4$OXYMAX,
-  GENUS_BGS = m4$GENUS_BGS,
-  SPEC_BGS = m4$SPEC_BGS,
-  CNT = m4$CNT,
-  CNTEXP = m4$CNTEXP
-)
